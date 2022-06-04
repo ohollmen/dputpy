a29be419f8e067d351d5ecd46dbb313c57227640
Olli Hollmen <olli.hollmen@gmail.com>
2022-06-04 12:18:46 -0700

    Add tests for indexer and CSV writer.

--- selftest.py
+++ selftest.py
@@ -4,12 +4,13 @@ import dputpy
 import json
 import dputpy.dputil as dputil
 import dputpy.gcputil as gcputil
+import dputpy.indexer as indexer
 print("Welcome to dputpy !");
 #y = dputpy.dputil.yamlload("./tdata/dict.yaml");
 y = dputil.yamlload("./tdata/dict.yaml");
 print(json.dumps(y, indent=2));
-y = dputil.yamlload("./tdata/multidoc.yaml", Xmulti=1);
-print(json.dumps(y, indent=2));
+yarr = dputil.yamlload("./tdata/multidoc.yaml", Xmulti=1);
+print(json.dumps(yarr, indent=2));
 
 res = dputil.run("ls -al /etc/passwd /etc/not_there");
 print(json.dumps(res, indent=2));
@@ -23,3 +24,17 @@ dputil.jsonwrite(jwt, jwtfn);
 print("Wrote: "+jwtfn);
 jwt = dputil.jsonload(jwtfn);
 if jwt: print("Was able to load JSON back !")
+
+# CSV
+fn_csv = "/tmp/multidoc.csv"
+err = dputil.csv_write(yarr, fn_csv, sep='\t')
+if err: print("Error "+err+" writing CSV"); exit(1)
+print("Wrote: "+fn_csv);
+
+idx = indexer.index(yarr, "ssn")
+print(json.dumps(idx, indent=2));
+yarr[1]["ssn"] = "001" # Set non-unique
+idx = indexer.index(yarr, "ssn")
+
+nucnt = indexer.nonunique(yarr, "ssn")
+print(str(nucnt)+" Non-unique item(s)");
