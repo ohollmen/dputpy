f9ad6322059dd0c66af611db7967ef4b4b1fac39
Olli Hollmen <olli.hollmen@gmail.com>
2023-09-26 11:40:02 -0700

    Add comments, exception handling, misc bulletproofing.

--- tmpltree.py
+++ tmpltree.py
@@ -1,6 +1,10 @@
+# dputpy template tree mapper/generator
+# 
 import os
 import jinja2
 import json
+#from pathlib import Path
+#import pathlib
 #NON-STD:from binaryornot.check import is_binary
 
 def init(cfg):
@@ -12,16 +16,19 @@ def init(cfg):
   cfg["params"] = json.loads(jcfg)
   return
 
+# Binary file detection (based on stack overflow post ref'd at call location)
 def is_bin(fn):
   textchars = bytearray({7,8,9,10,12,13,27} | set(range(0x20, 0x100)) - {0x7f})
   is_binary_string = lambda bytes: bool(bytes.translate(None, textchars))
   return is_binary_string(open(fn, 'rb').read(1024))
 
-def find_files(cfg):
+# Extract a list of files recursively from cfg["travroot"].
+# Pre-eliminates non-readble files and binary files.
+# Return list of files.
+# TODO: Allow list of dicts
+def find_files(cfg, **kwargs):
   troot = cfg.get("travroot")
   if not troot: raise "No travroot in config"
-  
-  
   fnames = []
   debug = 0
   for root, dirs, files in os.walk(troot): # "."
@@ -43,10 +50,29 @@ def find_files(cfg):
       # Seems detecting binary file is rather cumbersome in Python
       # Suggestions: https://stackoverflow.com/questions/898669/how-can-i-detect-if-a-file-is-binary-non-text-in-python
       if is_bin(src): print("BIN: "+src); continue
+      #if kwargs.get("lod"): fnames.append({"": "", "": "", "":""})
       fnames.append(src)
   return fnames
 
-def map_files(cfg, fnames):
+def ensure_path(dn, **kwargs):
+  exists = os.path.exists(dn)
+  isdir = os.path.isdir(dn)
+  # Check: ... As directory
+  if exists and isdir: return 0
+  if exists: print("PATH-ERROR: Is a file: "+dn+" expected a dir or nothing"); return 1
+  if (not exists) : # or (isdir)
+    if kwargs.get("debug"): print("Must create "+dn)
+    try:
+      #os.mkdir(dn)
+      os.makedirs(dn) # parents/nested. Seems to work
+      # Modern:
+      #Path(dn).mkdir(parents=True, exist_ok=True)
+    except OSError as error: #  
+      print("PATH-ERROR: Could not create path: "+dn+" - "+str(error))
+      return 1
+  return 0
+
+def map_files(cfg, fnames, **kwargs): # 
   travroot = cfg.get("travroot")
   if not travroot: raise "No travroot in config"
   tgtroot = cfg.get("tgtroot")
@@ -55,35 +81,53 @@ def map_files(cfg, fnames):
   p = cfg.get("params")
   # ... and object
   if not p: raise "No parameters for templating !"
-  runtmpl = 1
+  runtmpl = kwargs.get("runtmpl")
+  debug = kwargs.get("debug")
+  stats = {"except": 0, "bytecnt": 0, "filecnt": 0, "exfiles": []}
   for fn in fnames:
     relp = os.path.relpath(fn, travroot) # OLD: root, travroot
     tgtpath = tgtroot + relp # + "/"
-    print("# relpath: " + relp + " => tgtpath: " + tgtpath);
-    # + "/" + bn
-    if runtmpl:
-      #try:
-      cont = open(fn, "r").read() # "r" OR "rb" ?
-      print(cont)
-      tmpl = jinja2.Template(cont)
-      ocont = tmpl.render(**p)
-      print("Gen'd "+str(len(ocont)) + " B of content");
-      #except:
-      #  
+    debug and print("# relpath: " + relp + " => tgtpath: " + tgtpath)
     # Check dir of tgtpath
     dn = os.path.dirname(tgtpath)
-    print("TGT-DIR: "+dn)
-    if (not os.path.exists(dn)) or (os.path.isdir(dn)):
-      print("Must create "+dn)
-    # try:
-    #  os.mkdir(dn)
-    #except: OSError as error:
-    #  print("Could not create ...")
-    #if not
-    # Save-as open()
+    #print("TGT-DIR: "+dn)
+    derr = ensure_path(dn)
+    if derr: continue
+    # + "/" + bn
+    if runtmpl:
+      try:
+        cont = open(fn, "r").read() # "r" OR "rb" ?
+        if kwargs.get("showcont"): print(cont)
+        tmpl = jinja2.Template(cont)
+        ocont = tmpl.render(**p)
+        debug and print("Gen'd "+str(len(ocont)) + " B of content");
+        stats["bytecnt"] += len(ocont)
+        stats["filecnt"] += 1
+      except:
+        print("Could not create content from: "+ fn + " - ...")
+        stats["except"] += 1
+        stats["exfiles"].append(fn)
+    if kwargs.get("save"):
+      print("Should save output: "+tgtpath);
+      print(ocont);
+      # Save-as
+      try:
+        # With "wb": TypeError: a bytes-like object is required, not 'str'
+        open(tgtpath, "w").write(ocont)
+      except:
+        print("Error saving: "+tgtpath);
+  return stats
+# Example/testbed of using template tree mapper
 if __name__ == "__main__":
-  cfg = {"travroot": "/etc", "params": {"foo": 1}, "tgtroot": "/tmp/"}
+  cfg = {"travroot": "/etc", "params": {"foo": 1}, "tgtroot": "/tmp/gentest/"}
   init(cfg)
   fnames = find_files(cfg)
+  # Preferably filter files down in here to not run templating on files
+  # that are not actually templates. This should not hurt anything, but
+  # occasionally might trigger (e.g.) template syntax errors when template
+  # parser thinks it has found a template fragment, but pattern is actually
+  # just part of "other file" syntax.
   #print(json.dumps(fnames, indent=2))
-  map_files(cfg, fnames)
+  stats = map_files(cfg, fnames, runtmpl=1, save=1)
+  print(json.dumps(stats, indent=2))
+  print("Wrote all files to: "+cfg.get("tgtroot"));
