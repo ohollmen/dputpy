bdf835087a562a0b1ff84822dfb06d9c45788dd2
Olli Hollmen <olli.hollmen@gmail.com>
2024-04-26 19:09:43 -0700

    Mod setup.py to install properly.

--- setup.py
+++ setup.py
@@ -2,16 +2,22 @@
 # Tentative (WIP) setup.py for dputpy.
 # https://docs.python.org/3/distutils/setupscript.html
 # Optionally have setup.cfg (ini file)
+# https://packaging.python.org/en/latest/guides/distributing-packages-using-setuptools/
 from distutils.core import setup
 
 setup(name='dputpy', version='1.0',author='Olli Hollmen',
   description='DevOps Data Processing Utility Toolkit (Python Edition)',
   url='',
-  py_modules=['dputil', 'indexer'],
+  # Only for  single-file Python modules that aren’t part of a package
+  #py_modules=['dputil', 'indexer'],
   # Full packages found in setup.py root
   # Must have foo/__init__.py
   #packages=['foo', 'foo.command'],
+  packages=['dputpy'],
   #package_dir={'mypkg': 'src/mypkg'},
-  #scripts=['scripts/a', 'scripts/b'],
+  package_dir={'dputpy': '.'},
+  #scripts=['bin/drproc.py', 'scripts/b'],
+  scripts=['bin/drproc.py', 'bin/podlist.py', 'tmplgen.py'],
   #data_files=[('', [])
+  #install_requires=['']
 )
