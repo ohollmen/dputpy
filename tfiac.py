4c47f7bea2477a98dacdc7969f637f7701c5b277
Olli Hollmen <olli.hollmen@gmail.com>
2025-07-01 08:33:43 -0700

    Add some tf (state) accessors.

--- tfiac.py
+++ tfiac.py
@@ -5,6 +5,7 @@ import hcl2
 import json
 import argparse
 import dputpy.clapp as clapp
+import dputpy.dputil as dputil
 import sys # sys.argv
 # Load and Parse TF HCL2 file.
 # Data (by hcl2 library) is laid out so that e.g. multiple individual "variable" declarations
@@ -50,6 +51,59 @@ def tftrans(opts):
   arr = tfvars_simplify(arr)
   print(json.dumps(arr, indent=2));
   return
+
+# Pull terraform state
+def st_pull(**kwargs):
+  bcmd = "terraform"
+  if kwargs.get("basecmd"): bcmd = kwargs.get("basecmd")
+  cmd = f"{bcmd} state pull"
+  # Run and parse ...
+  st = None
+  ret = dputil.run(cmd, fmt="json", debug=1) # onerror
+  if ret.get("rc"): raise ValueError(f"Error {ret.get('rc')} in running {cmd}")
+  st = ret.get("out")
+  if not st: raise ValueError("Could not parse TF State JSON")
+  return st
+def state_rm(**kwargs): # Pass type(name, str), elem(dict) ?
+  bcmd = "terraform"
+  if kwargs.get("basecmd"): bcmd = kwargs.get("basecmd")
+  cmd = f"{bcmd} state rm"
+  # ret = dputil.run(cmd,  debug=1) # fmt="json", onerror
+  # if ret.get("rc"): 
+  return
+# Access resource branch in terraform state (st)
+# Allow
+# TODO: raise ValueError() on failed access
+def st_get(st, key, etype):
+  if not st: return None
+  if not key: raise ValueError("No key passed !")
+  # Pre-populate some
+  etypes = st.get("resources")
+  if not etypes: return None
+  if not isinstance(etypes, dict): return None
+  # Type list
+  if etype == 'rtypes':
+    return etypes
+  ############################## 
+  # Instance ?
+  etypes.get(key);
+  return None
+# Get instance list for particular TF type.
+# On caller side the next likely step is to get instance (e.g.):
+# ```
+#  instlist = tfiac.st_instance_list(st, "my_tf_type")
+#  key = ""
+#  inst_l = list(filter(lambda(x): ..., instlist))
+#  if len(ilist_l) != 1: raise ValueError(f"'{key}' - Not a singular instance !")
+#  # E.g. Get name, run state rm ...
+#  #
+# ```
+def st_instance_list(st, etype):
+  ilist = st_get(st, key, 'inst')
+  #if not ilist: raise ValueError(f"Could not get instance list for type {etype}")
+  return ilist
+#######################################################
+
 def usage(msg):
   if msg: print(msg)
   exit(1)
@@ -67,6 +121,7 @@ if __name__ == "__main__":
   opts = vars(pargs) # sys.argv / sys.argv[1:]
   #print(opts);print(rawargs);exit(0);
   #opts = {} # {"fn": sys.argv[1]}
-  opts["fn"] = rawargs[0] # sys.argv[1]
+  if len(rawargs): opts["fn"] = rawargs[0] # sys.argv[1]
+  else: print("No args !"); exit(1)
   tftrans(opts)
   exit(0)
